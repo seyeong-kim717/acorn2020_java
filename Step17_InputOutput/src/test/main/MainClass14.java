5232ed21124c0def043876e4a962d60c6c048a89
seo <sykim1123@nate.com>
2020-06-16 12:05:17 +0900

    6월 16일 오전 13 14번 수정.

--- Step17_InputOutput/src/test/main/MainClass14.java
+++ Step17_InputOutput/src/test/main/MainClass14.java
@@ -1,5 +1,6 @@
 package test.main;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,22 +9,21 @@ public class MainClass14 {
 	public static void main(String[] args) {
 		File memoFile=new File("c:/acorn2020/myFolder/memo.txt");
 		try {
-			if (!memoFile.exists()) {
-				System.out.println("파일이 없어요!");
-				return; // 메소드 끝내기
+			if(!memoFile.exists()) {
+				System.out.println("파일이 존재 하지 않습니다.");
+				return; //메소드 끝내기 
 			}
-			//파일에서 문자열 읽어들일 객체
+			//파일에서 문자열을 읽어들일 객체
 			FileReader fr=new FileReader(memoFile);
+			BufferedReader br=new BufferedReader(fr);
 			while(true) {
-				//글자 하나 하나의 코드값을 반복문 돌면서 읽어 들인다
-				int code=fr.read();//더이상 읽을 코드가 없으면.
-				if(code==-1) {//반복문 탈출
-					break;
+				//반복문 돌면서 문자열을 줄단위로(개행기호 기준) 읽어낸다.
+				String line=br.readLine();
+				if(line==null) {//더이상 읽을 문자열이 없으면
+					break;//반복문 탈출
 				}
-				//코드값에 해당되는 문자
-				char ch=(char)code;
-				//콘솔창에 개행 기호 없이 한글자 한글자 출력하기
-				System.out.print(ch);
+				//읽은 문자열 출력하기 
+				System.out.println(line);
 			}
 		}catch(IOException ie) {
 			ie.printStackTrace();
