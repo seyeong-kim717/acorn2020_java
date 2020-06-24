cd8cd17f87ef9ac1a2d1b5ef8bcd7ee1ecd4eada
seo <sykim1123@nate.com>
2020-06-24 16:05:46 +0900

    memo 수정

--- Step19_JDBC/src/test/dto/MemoDto.java
+++ Step19_JDBC/src/test/dto/MemoDto.java
@@ -1,11 +1,10 @@
 package test.dto;
 
 public class MemoDto {
-	//회원 한명의 정보를 담을 필드 선언
 	private int num;
 	private String content;
 	private String regdate;
-	//default 생성자 만들기
+	public MemoDto() {}
 	public MemoDto(int num, String content, String regdate) {
 		super();
 		this.num = num;
